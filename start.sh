a92b8f85e7bcb12bdc4c966c68a3fc552fad4061
KairigoShawn <kairigonjogu@outlook.com>
2022-01-21 01:34:05 +0300

    populates start file to enable running of project

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY="5431"
+export MAIL_PASSWORD="5431"
+export MAIL_USERNAME="morrison.githinji@student.moringaschool.com"
+export API_KEY="http://quotes.stormconsultancy.co.uk/random.json (Links to an external site.)"
+
+python3.9 manage.py server
